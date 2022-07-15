e79e974bf0749ac0c0381ca9ee9fd4992999df66
studentshivang <studentshivang>
2022-07-15 09:13:42 +0530

    fourth commit

---
+++ arrayAndObjects2.js
@@ -0,0 +1,21 @@
+//Mapping ❤
+let arr=[ 'value 1', 'value 2', 'mutated value 3', 'value 4' ];
+
+arr.map((v) => console.log("addded", v));
+
+const ages = [32, 33, 16, 40];
+//deleting array element - filter method
+
+console.log(ages.filter(v=>v!=33));
+
+// OR
+
+console.log(ages.filter((ele,index)=>{
+    return index!=1;
+}));
+
+/*filter method creates a new array
+(filled) with elements that pass a test provided by a function.*/
+//filter() method does not change the original array.
+
+
